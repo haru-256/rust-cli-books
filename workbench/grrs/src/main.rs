eb750458663e9e569f293fa437712bef48f459bc
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-08-25 20:15:07 +0900

    FEAT: add logger

--- workbench/grrs/src/main.rs
+++ workbench/grrs/src/main.rs
@@ -1,8 +1,11 @@
 use anyhow::{self, Context};
 use clap::Parser;
+use log::{error, info};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -29,24 +32,40 @@ fn validate_path(s: &str) -> Result<PathBuf, String> {
 }
 
 fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
     let args = Cli::parse();
 
     let f =
         File::open(&args.path).with_context(|| format!("failed to open file: {:?}", &args.path))?;
     let br = BufReader::new(f);
 
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
+
+    info!(
+        "searching for pattern: {} from file: {:?}",
+        args.pattern, args.path
+    );
+    let pb = indicatif::ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(100));
     for line in br.lines() {
         let content = match line {
             Ok(l) => l,
             Err(e) => {
+                error!("failed to read line: {:?}", e);
                 return Err(e.into());
             }
         };
 
         if content.contains(&args.pattern) {
-            println!("{}", content);
+            writeln!(handle, "{}", content)?;
         }
+        thread::sleep(Duration::from_secs(1));
     }
+    pb.finish_and_clear();
+
+    handle.flush()?;
 
     Ok(())
 }
